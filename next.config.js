6ae93c20355c81e4ef1ce00985efa828c13a66ed
Varun Tiwari <tiwarivarun819.vt33.vt@gmail.com>
2022-10-04 14:47:48 +0530

    Added env variables to next.config.js

--- next.config.js
+++ next.config.js
@@ -9,7 +9,25 @@ const nextConfig = {
             'i.ibb.co',
         ],
     },
-    
+    env: {
+        IMGBB_API_KEY: process.env.IMGBB_API_KEY,
+        TYPE: process.env.TYPE,
+        PROJECT_ID: process.env.PROJECT_ID,
+        PRIVATE_KEY_ID: process.env.PRIVATE_KEY_ID,
+        PRIVATE_KEY: process.env.PRIVATE_KEY,
+        CLIENT_EMAIL: process.env.CLIENT_EMAIL,
+        CLIENT_ID: process.env.CLIENT_ID,
+        AUTH_URI: process.env.AUTH_URI,
+        TOKEN_URI: process.env.TOKEN_URI,
+        AUTH_PROVIDER_X509_CERT_URL: process.env.AUTH_PROVIDER_X509_CERT_URL,
+        CLIENT_X509_CERT_URL: process.env.CLIENT_X509_CERT_URL,
+        APIKEY: process.env.APIKEY,
+        AUTHDOMAIN: process.env.AUTHDOMAIN,
+        PROJECTID: process.env.PROJECTID,
+        STORAGEBUCKET: process.env.STORAGEBUCKET,
+        MESSAGINGSENDERID: process.env.MESSAGINGSENDERID,
+        APPID: process.env.APPID,
+    },
 }
 
 module.exports = nextConfig
