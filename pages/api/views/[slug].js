5fd942a290d1238a3fcc03f42f8687620a585ae4
Suyash Vikram Singh <suyashvikramsingh2002@gmail.com>
2022-10-06 01:16:42 +0530

    Added code in [slug].js

--- pages/api/views/[slug].js
+++ pages/api/views/[slug].js
@@ -1,16 +1,34 @@
-// TODO: implement serverless function to increment view counter
-/**
- * @param {import('next').NextApiRequest} req
- * @param {import('next').NextApiResponse} res
- * @returns {Promise<void>}
- * @see https://nextjs.org/docs/api-routes/introduction
- * @see https://nextjs.org/docs/api-routes/api-middlewares
- * 
- * *create a db collection "views" with a document for each post
- * *create a field "viewCount" in the document
- * *increment the viewCount field by 1 when the post is viewed
- * *use the post slug as the document id
- * *use the firebase-admin sdk to access the db
- * *use the firebase-admin sdk to increment the viewCount field
- *  
- */
+import { db } from '../../../lib/firebase'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default async (req, res) => {
+    if (req.method === 'POST') {
+        const { slug } = req.query
+        const ref = db.collection('views').doc(slug)
+        // if doc exists, increment views by 1 else create doc with views = 1
+        const doc = await ref.get()
+        if (doc.exists) {
+            await ref.update({
+                views: doc.data().views + 1,
+            })
+        } else {
+            await ref.set({
+                views: 1,
+            })
+        }
+        //send view count to client
+        const newDoc = await ref.get()
+        return res.status(200).json({ views: newDoc.data().views })
+    }
+    if (req.method === 'GET') {
+        const { slug } = req.query
+        const ref = db.collection('views').doc(slug)
+        const doc = await ref.get()
+        if (doc.exists) {
+            return res.status(200).json({ views: doc.data().views })
+        } else {
+            return res.status(200).json({ views: 0 })
+        }
+    }
+    res.status(400).json({ message: 'Invalid request' })
+}
